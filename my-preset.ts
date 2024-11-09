f706b70309112a65b71ecc013f1072b1da260dd3
AmirrezaRezaeii <amirrezarezaeijavidan@gmail.com>
2024-11-09 22:45:36 +0330

    Customizing tailwind with preset

---
+++ my-preset.ts
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        light: "#85d7ff",
+        DEFAULT: "#1fb6ff",
+        dark: "#009eeb",
+      },
+      secondry: {
+        light: "#ff7ce5",
+        DEFAULT: "#ff49db",
+        dark: "#ff16d1",
+      },
+    },
+    extend: {
+      flexGrow: {
+        2: "2",
+        3: "3",
+      },
+      zIndex: {
+        60: "60",
+        70: "70",
+        80: "80",
+        90: "90",
+        100: "100",
+      },
+    },
+  },
+};
