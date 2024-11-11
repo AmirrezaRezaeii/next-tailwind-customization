3af7a2fd3a21e5f580202e8e90d5696b46cb7d41
AmirrezaRezaeii <amirrezarezaeijavidan@gmail.com>
2024-11-11 11:35:36 +0330

    Disabling corePlugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,51 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  presets: [require('./my-preset.ts')], 
+
+  // Customizing via preset file:
+
+  // presets: [require('./my-preset.ts')],
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 
-  // Customizing via theme option
+  // Customizing via theme option:
 
-  // theme: {
-  //   colors: {
-  //     primary: {
-  //       light: "#85d7ff",
-  //       DEFAULT: "#1fb6ff",
-  //       dark: "#009eeb",
-  //     },
-  //     secondry: {
-  //       light: "#ff7ce5",
-  //       DEFAULT: "#ff49db",
-  //       dark: "#ff16d1",
-  //     },
-  //   },
-  //   extend: {
-  //     flexGrow: {
-  //       2: "2",
-  //       3: "3",
-  //     },
-  //     zIndex: {
-  //       60: "60",
-  //       70: "70",
-  //       80: "80",
-  //       90: "90",
-  //       100: "100",
-  //     },
-  //   },
-  // },
+  theme: {
+    colors: {
+      primary: {
+        light: "#85d7ff",
+        DEFAULT: "#1fb6ff",
+        dark: "#009eeb",
+      },
+      secondry: {
+        light: "#ff7ce5",
+        DEFAULT: "#ff49db",
+        dark: "#ff16d1",
+      },
+    },
+    extend: {
+      flexGrow: {
+        2: "2",
+        3: "3",
+      },
+      zIndex: {
+        60: "60",
+        70: "70",
+        80: "80",
+        90: "90",
+        100: "100",
+      },
+    },
+  },
+
+  // This will disable every accentColor classes from tailwind import
+
+  corePlugins: {
+    accentColor: false
+  },
   plugins: [],
 } satisfies Config;
